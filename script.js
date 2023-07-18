54553460621935e872a027af7321b595ab95398c
abxzok379 <129567055+abxzok379@users.noreply.github.com>
2023-07-18 01:17:22 -0400

    Mise en ligne (incomplet)

---
+++ script.js
@@ -0,0 +1,27 @@
+
+
+
+
+
+function maFonction(){
+var cell, ligne, boutton;
+// Récupère l'id de la table qui sera modifié
+var tableau = document.getElementById("tableau01");
+var nbLignes = "penis";
+boutton = document.createElement("input");
+boutton.type = "button";
+boutton.className = "boutton";
+boutton.innerHTML = "fsdf";
+
+ligne = tableau.insertRow(-1);
+
+cell = ligne.insertCell(0);
+cell.innerHTML =  document.getElementById("inputNom").value;
+cell = ligne.insertCell(1);
+cell.innerHTML = document.getElementById("inputPrix").value;
+cell = ligne.insertCell(2);
+cell.appendChild(boutton);
+document.querySelector(".boutton").innerHTML = "Supprimer";
+
+
+}
